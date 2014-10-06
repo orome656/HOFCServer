cfd6c00461d0100092fc0eccee4fd292c54c8bc2
Orome656 <anthony.maladot@gmail.com>
2014-10-06 13:52:06 +0200

    Correction URL + correction serveur

--- server_sqlite.js
+++ server_sqlite.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database('../database_hofc.db');
+var db = new sqlite3.Database('database_hofc.db');
 
 app.get('/classement', function(req, res){
-    db.get('select * from classement', function (err, results) {
+    db.all('select * from classement', function (err, results) {
         res.send(results);
     });
 });
 
 app.get('/calendrier', function(req, res){
-    db.get('select * from calendrier', function (err, results) {
+    db.all('select * from calendrier', function (err, results) {
         res.send(results);
     });
 });
 
 app.get('/actus', function(req, res){
-    db.get('select * from actus', function (err, results) {
+    db.all('select * from actus', function (err, results) {
         res.send(results);
     });
 });
