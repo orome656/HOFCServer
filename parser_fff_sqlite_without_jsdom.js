c2b104957f20eaea29aeadf401f9c0878d71d5c1
Orome656 <anthony.maladot@gmail.com>
2014-10-09 14:05:16 +0200

    JSLint Correction

--- parser_fff_sqlite_without_jsdom.js
+++ parser_fff_sqlite_without_jsdom.js
@@ -17,24 +17,24 @@ var urlCalendrier = "http://www.fff.fr/la-vie-des-clubs/177005/calendrier/liste-
 var urlActus = "http://www.hofc.fr/category/seniors/";
 
 var optionsClassement = {
-  host: '',
-  port: ,
-  path: 'http://www.fff.fr/championnats/fff/district-hautes-pyrenees/2014/305257-excellence/phase-1/poule-1/derniers-resultats',
-  activated: false
+    host: '',
+    port: 80,
+    path: 'http://www.fff.fr/championnats/fff/district-hautes-pyrenees/2014/305257-excellence/phase-1/poule-1/derniers-resultats',
+    activated: false
 };
 
 var optionsCalendrier = {
-  host: '',
-  port: ,
-  path: 'http://www.fff.fr/la-vie-des-clubs/177005/calendrier/liste-matchs-a-venir/305257/phase-1/groupe-1',
-  activated: false
+    host: '',
+    port: 80,
+    path: 'http://www.fff.fr/la-vie-des-clubs/177005/calendrier/liste-matchs-a-venir/305257/phase-1/groupe-1',
+    activated: false
 };
 
 var optionsActus = {
-  host: '',
-  port: ,
-  path: 'http://www.hofc.fr/category/seniors/',
-  activated: true
+    host: '',
+    port: 80,
+    path: 'http://www.hofc.fr/category/seniors/',
+    activated: true
 };
 
 /**
@@ -48,42 +48,43 @@ var creation_table_actus_query = "CREATE TABLE IF NOT EXISTS `actus` (`id` INTEG
  *	Tableau permettant de convertir la chaine date récupérée en objet date
  */
 var listeMois = {
-        "JANVIER":"01",
-        "FÉVRIER":"02",
-        "MARS":"03",
-        "AVRIL":"04",
-        "MAI":"05",
-        "JUIN":"06",
-        "JUILLET":"07",
-        "AOUT":"08",
-        "SEPTEMBRE":"09",
-        "OCTOBRE":"10",
-        "NOVEMBRE":"11",
-        "DÉCEMBRE":"12"
-};
+        "JANVIER": "01",
+        "FÉVRIER": "02",
+        "MARS": "03",
+        "AVRIL": "04",
+        "MAI": "05",
+        "JUIN": "06",
+        "JUILLET": "07",
+        "AOUT": "08",
+        "SEPTEMBRE": "09",
+        "OCTOBRE": "10",
+        "NOVEMBRE": "11",
+        "DÉCEMBRE": "12"
+    };
 
 /**
  *	Tableau permettant de convertir la chaine date récupérée en objet date pour les actus
  */
 var listeMoisActu = {
-        "janvier":"01",
-        "février":"02",
-        "mars":"03",
-        "avril":"04",
-        "mai":"05",
-        "juin":"06",
-        "juillet":"07",
-        "août":"08",
-        "septembre":"09",
-        "octobre":"10",
-        "novembre":"11",
-        "décembre":"12"
-};
+        "janvier": "01",
+        "février": "02",
+        "mars": "03",
+        "avril": "04",
+        "mai": "05",
+        "juin": "06",
+        "juillet": "07",
+        "août": "08",
+        "septembre": "09",
+        "octobre": "10",
+        "novembre": "11",
+        "décembre": "12"
+    };
 
 /**
  *
  */
 var doAfterQuery = function (errors, resultats) {
+    "use strict";
 	if (errors) {
 		winston.error('Erreur de mise a jour ' + errors);
 		return;
@@ -93,47 +94,48 @@ var doAfterQuery = function (errors, resultats) {
 
 winston.info('Parser start at ' + new Date());
 
-if(optionsClassement.activated) {
+if (optionsClassement.activated) {
 	winston.info('Parser Classement Start');
 	http.get(optionsClassement, function(res) {
+        "use strict";
 		var result = "";
-		if(res.statusCode != 200) {
+		if (res.statusCode != 200) {
 			return;
 		}
-	    res.on('data', function(data) {
+	    res.on('data', function (data) {
 			result += data;
 	    });
 		
-		res.on('end', function() {
+		res.on('end', function () {
 			$ = cheerio.load(result);
-			db.serialize(function() {
+			db.serialize(function () {
 				db.run(creation_table_classement_query);
 				var linesClassement = $("table.classement").children('tbody').children().filter(function (index) {
-					return ($(this).children() != null && $(this).children().length > 3);
+					return ($(this).children() !== null && $(this).children().length > 3);
 				});
 				var nbLines = linesClassement.length;
 				$(linesClassement).each(function (index, line) {
 					var lineChildren = $(line).children();
 					if (lineChildren != null && lineChildren.length > 3) {
-						var nom = $(lineChildren[1]).text().trim();
-						var points = $(lineChildren[2]).text();
-						var joue = $(lineChildren[3]).text();
-						var victoire = $(lineChildren[4]).text();
-						var nul = $(lineChildren[5]).text();
-						var defaite = $(lineChildren[6]).text();
-						var bp = $(lineChildren[8]).text();
-						var bc = $(lineChildren[9]).text();
-						var diff = $(lineChildren[11]).text();
+						var nom = $(lineChildren[1]).text().trim(),
+						    points = $(lineChildren[2]).text(),
+						    joue = $(lineChildren[3]).text(),
+						    victoire = $(lineChildren[4]).text(),
+						    nul = $(lineChildren[5]).text(),
+						    defaite = $(lineChildren[6]).text(),
+						    bp = $(lineChildren[8]).text(),
+						    bc = $(lineChildren[9]).text(),
+						    diff = $(lineChildren[11]).text();
 						db.get('select * from classement where nom LIKE "' + nom + '"',  function (err, results) {
 							if (err) {
 								winston.info('Erreur ' + err);
 								return;
 							}
                             winston.info('Updating Classement for team ' + nom);
-							if (results != null) {
-								db.run('UPDATE classement set points=' + points + ', joue=' + joue + ', gagne=' + victoire + ', nul=' + nul + ', perdu=' + defaite + ', bp=' + bp + ', bc=' + bc + ', diff=' + diff + ' WHERE nom LIKE "' + nom + '"',doAfterQuery);
+							if (results !== null) {
+								db.run('UPDATE classement set points=' + points + ', joue=' + joue + ', gagne=' + victoire + ', nul=' + nul + ', perdu=' + defaite + ', bp=' + bp + ', bc=' + bc + ', diff=' + diff + ' WHERE nom LIKE "' + nom + '"', doAfterQuery);
 							} else {
-								db.run('insert into classement (nom,points,joue,gagne,nul,perdu,bp,bc,diff) VALUES ("' + nom + '",' + points + ',' + joue + ' , ' + victoire + ',' + nul + ',' + defaite + ', ' + bp + ',' + bc + ',' + diff + ')',doAfterQuery);
+								db.run('insert into classement (nom,points,joue,gagne,nul,perdu,bp,bc,diff) VALUES ("' + nom + '",' + points + ',' + joue + ' , ' + victoire + ',' + nul + ',' + defaite + ', ' + bp + ',' + bc + ',' + diff + ')', doAfterQuery);
 							}
 						});
 					}
@@ -141,12 +143,13 @@ if(optionsClassement.activated) {
 			});
 		});
 	}).on('error', function(e) {
+        "use strict";
 	    winston.error("Got error: " + e.message);
 	});
 }
 
 
-if(optionsCalendrier.activated) {
+if (optionsCalendrier.activated) {
 	winston.info('Parser Calendrier Start');
 	http.get(optionsCalendrier, function(res) {
 	  var result = "";
@@ -162,36 +165,39 @@ if(optionsCalendrier.activated) {
 			$2 = cheerio.load(result);
 			db.serialize(function() {
 				db.run(creation_table_calendrier_query);
-				var linesCalendar = $2("div.list_calendar").children('div');
-			    var nbLines = linesCalendar.length;
+				var linesCalendar = $2("div.list_calendar").children('div'),
+			        nbLines = linesCalendar.length;
 			    winston.info(nbLines);
 			    $2(linesCalendar).each(function (index, line) {
-				    var lineChildren = $2(line).children();
-				    var date = $2(lineChildren[0]).text().trim();
-				    var equipe1 = $2($2(lineChildren[1]).children()[0]).text().trim();
+				    var lineChildren = $2(line).children(),
+				        date = $2(lineChildren[0]).text().trim(),
+				        equipe1 = $2($2(lineChildren[1]).children()[0]).text().trim();
+                    
 				    equipe1 = equipe1.replace(/\r?\n|\r/g,' ');
 				    equipe1 = equipe1.replace(/ +/g,' ');
 					winston.info('equipe1 ' + equipe1);
-					var equipe2 = $2($2(lineChildren[1]).children()[2]).text().trim();
+					
+                    var equipe2 = $2($2(lineChildren[1]).children()[2]).text().trim();
 					equipe2 = equipe2.replace(/\r?\n|\r/g,' ');
 					equipe2 = equipe2.replace(/ +/g,' ');
 					winston.info('equipe2 ' + equipe2);
 				  
-				    var jourComplet = date.split('-')[0];
-				    var heureComplet = date.split('-')[1];
-				    var jour = jourComplet.split(' ')[1];
-				    var mois = listeMois[jourComplet.split(' ')[2].toUpperCase()];
-				    var annee = jourComplet.split(' ')[3];
-				    var heure = "00";
-				    var minute = "00";
+				    var jourComplet = date.split('-')[0],
+				        heureComplet = date.split('-')[1],
+				        jour = jourComplet.split(' ')[1],
+				        mois = listeMois[jourComplet.split(' ')[2].toUpperCase()],
+				        annee = jourComplet.split(' ')[3],
+				        heure = "00",
+				        minute = "00";
+                    
 				    if(heureComplet) {
 					    heure = heureComplet.split(':')[0];
 					    minute = heureComplet.split(':')[1];
 				    }
 
-					var score = $2($2(lineChildren[1]).children()[1]).text().trim();
-				    var score1 = null;
-				    var score2 = null;
+					var score = $2($2(lineChildren[1]).children()[1]).text().trim(),
+				        score1 = null,
+				        score2 = null;
 
 					if (score.indexOf('-') != -1) {
 						score1 = score.split('-')[0];
@@ -205,11 +211,11 @@ if(optionsCalendrier.activated) {
 						}
                         winston.info('Updating Calendrier for match ' + equipe1 + ' - ' + equipe2);
 						if (results != null) {
-							db.run('UPDATE calendrier set date="' + annee+'-'+mois+'-'+jour + ' ' + heure + ':'
-									+ minute + ':00' + '", score1=' + score1 + ', score2=' + score2 + ' WHERE equipe1 LIKE "' + equipe1 + '" AND equipe2 LIKE "' + equipe2 + '"',doAfterQuery);
+							db.run('UPDATE calendrier set date="' + annee + '-' + mois + '-' + jour + ' ' + heure + ':'
+									+ minute + ':00' + '", score1=' + score1 + ', score2=' + score2 + ' WHERE equipe1 LIKE "' + equipe1 + '" AND equipe2 LIKE "' + equipe2 + '"', doAfterQuery);
 						} else {
-							db.run('insert into calendrier (date,equipe1,equipe2,score1,score2) VALUES ("' + annee+'-'+mois+'-'+jour + ' ' + heure + ':'
-									+ minute + ':00' + '","' + equipe1 + '","' + equipe2 + '","' + score1 + '","' + score2 + '")',doAfterQuery);
+							db.run('insert into calendrier (date,equipe1,equipe2,score1,score2) VALUES ("' + annee + '-' + mois + '-' + jour + ' ' + heure + ':'
+									+ minute + ':00' + '","' + equipe1 + '","' + equipe2 + '","' + score1 + '","' + score2 + '")', doAfterQuery);
 						}
 
 					});
@@ -221,7 +227,7 @@ if(optionsCalendrier.activated) {
 	});
 }
 
-if(optionsActus.activated) {
+if (optionsActus.activated) {
     winston.info('Parser actus start at ' + new Date());
 	http.get(optionsActus, function(res) {
 	    var result = "";
@@ -250,37 +256,38 @@ if(optionsActus.activated) {
 					 *      text: titre de l'actu
 					 * }
 					 */
-					var title = $3(line).children('.title').children().children();
-					var date = $3(line).children('.postmeta').children('span').text();
-					var urlImage = $3(line).children('.entry').children('a').children().attr('src');
-					var texte = $3(line).children('.entry').children('p').text();
-					
-					var jour = date.split(' ')[0];
-                    if(jour.length == 1) {
-                        jour = "0"+jour;   
+					var title = $3(line).children('.title').children().children(),
+					    date = $3(line).children('.postmeta').children('span').text(),
+					    urlImage = $3(line).children('.entry').children('a').children().attr('src'),
+					    texte = $3(line).children('.entry').children('p').text(),
+					    jour = date.split(' ')[0];
+                    
+                    if (jour.length === 1) {
+                        jour = '0' + jour;
                     }
-					var mois = listeMoisActu[date.split(' ')[1]];
-					var annee = date.split(' ')[2];
+                    
+					var mois = listeMoisActu[date.split(' ')[1]],
+					    annee = date.split(' ')[2];
                     
 					db.run('select * from actus where postId=' + postId, function (err, results) {
 						if (err) {
 							winston.info('Erreur ' + err);
 							return;
 						}
-						if (results != null) {
-                            var query = 'update actus set titre=?, texte=?, url=?, image=?, date=? WHERE postId=?';
-                            var parameters = [title.text(),texte,title.attr('href'), urlImage, annee+'-'+mois+'-'+jour + ' 00:00:00', postId];
+						if (results !== null) {
+                            var query = 'update actus set titre=?, texte=?, url=?, image=?, date=? WHERE postId=?',
+                                parameters = [title.text(), texte, title.attr('href'), urlImage, annee + '-' + mois + '-' + jour + ' 00:00:00', postId];
                             
                             winston.info('Updating actus postId = ' + postId + ' with parameters : ' + parameters);
 							
-                            db.run(query, parameters,doAfterQuery);
+                            db.run(query, parameters, doAfterQuery);
 						} else {
-                            var query = 'insert into actus (postId, titre, texte, url, image, date) VALUES (?,?,?,?,?,?)';
-                            var parameters = [postId, title.text(),texte,title.attr('href'), urlImage, annee+'-'+mois+'-'+jour + ' 00:00:00'];
+                            var query = 'insert into actus (postId, titre, texte, url, image, date) VALUES (?,?,?,?,?,?)',
+                                parameters = [postId, title.text(), texte, title.attr('href'), urlImage, annee + '-' + mois + '-' + jour + ' 00:00:00'];
                             
                             winston.info('Inserting actus postId = ' + postId + ' with parameters : ' + parameters);
                         
-							db.run(query, parameters,doAfterQuery);
+							db.run(query, parameters, doAfterQuery);
 						}
 					});
 				});
