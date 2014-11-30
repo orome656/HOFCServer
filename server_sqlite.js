4702b949563061d218844f1a41109e7b8039c5f0
orome656 <anthony.maladot@gmail.com>
2014-11-30 20:46:04 +0100

    Start Notification WS implementation

--- server_sqlite.js
+++ server_sqlite.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database('database_hofc.db');
@@ -6,6 +7,9 @@ var db = new sqlite3.Database('database_hofc.db');
 var CronJob = require('cron').CronJob
 var parser = require('./parser_node_module.js');
 var winston = require("winston");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var logger = new (winston.Logger)({
     transports: [
@@ -18,6 +22,7 @@ var logger = new (winston.Logger)({
 //winston.add(winston.transports.File, { filename: './server.log', level: 'debug'});
 //winston.remove(winston.transports.Console);
 
+var creation_table_notification_query = "CREATE TABLE IF NOT EXISTS `notification_client` (`id` INTEGER PRIMARY KEY AUTOINCREMENT , `uuid` varchar(255) NOT NULL, `notification_id` varchar2(50) NOT NULL)";
 
 //var j = schedule.scheduleJob('*/10 * * * *', function(){
 //    logger.info('Launching Database Update');
@@ -30,7 +35,7 @@ var job = new CronJob('0 */15 * * * *', function(){
   }, function () {
     
   },
-  true /* Start the job right now */
+  true // Start the job right now 
 );
 
 app.get('/classement', function(req, res){
@@ -51,6 +56,18 @@ app.get('/actus', function(req, res){
 	logger.info('Actus Request');
     db.all('select * from actus', function (err, results) {
         res.send(results);
+    });
+});
+
+app.post('/registerPush', function(req, res){
+	logger.info('New registration ');
+    db.serialize(function () {
+        db.run(creation_table_notification_query);
+        db.get("SELECT * FROM notification_client where uuid='" + req.body.uuid +"'", function(result){
+            if(result == null) {
+                
+            }
+        });
     });
 });
 
