184ac401d34fdf0c8ebe13c0db7c51d2a91743df
orome656 <anthony.maladot@gmail.com>
2015-05-22 22:30:26 +0200

    models

--- models/classementLine.ts
+++ models/classementLine.ts
@@ -9,4 +9,65 @@ export class ClassementLine {
 	private _bp: number;
 	private _bc: number;
 	private _diff: number;
+	
+	get id(): number {
+		return this._id;
+	}
+	set id(id: number) {
+		this._id = id;
+	} 
+	get nom(): string {
+		return this._nom;
+	}
+	set nom(nom: string) {
+		this._nom = nom;
+	} 
+	get joue(): number {
+		return this._joue;
+	}
+	set joue(joue: number) {
+		this._joue = joue;
+	} 
+	get points(): number {
+		return this._points;
+	}
+	set points(points: number) {
+		this._points = points;
+	} 
+	get gagne(): number {
+		return this._joue;
+	}
+	set gagne(gagne: number) {
+		this._gagne = gagne;
+	} 
+	get nul(): number {
+		return this._nul;
+	}
+	set nul(nul: number) {
+		this._nul = nul;
+	} 
+	get perdu(): number {
+		return this._perdu;
+	}
+	set perdu(perdu: number) {
+		this._perdu = perdu;
+	} 
+	get bp(): number {
+		return this._bp;
+	}
+	set bp(bp: number) {
+		this._bp = bp;
+	} 
+	get bc(): number {
+		return this._bc;
+	}
+	set bc(bc: number) {
+		this._bc = bc;
+	} 
+	get diff(): number {
+		return this._diff;
+	}
+	set diff(diff: number) {
+		this._diff = diff;
+	}
 }
--- models/matchInfos.ts
+++ models/matchInfos.ts
@@ -3,4 +3,35 @@ export class MatchInfos {
 	private _adresse: string;
 	private _ville: string;
 	private _arbitres: Array<string>;
+	
+	public get nom() : string {
+		return this._nom;
+	}
+	
+	public set nom(v : string) {
+		this._nom = v;
+	}
+	
+	public get adresse() : string {
+		return this._adresse;
+	}
+	
+	public set adresse(v : string) {
+		this._adresse = v;
+	}
+	public get ville() : string {
+		return this._ville;
+	}
+	
+	public set ville(v : string) {
+		this._ville = v;
+	}
+	public get arbitres() : Array<string> {
+		return this._arbitres;
+	}
+	
+	public set arbitres(v : Array<string>) {
+		this._arbitres = v;
+	}
+	
 }
