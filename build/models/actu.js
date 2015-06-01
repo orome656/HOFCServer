7980b6dedac11bb639b0266ac5ec3f11621b97fb
Orome656 <anthony.maladot@gmail.com>
2015-06-01 17:58:02 +0200

    Code Regeneration

--- build/models/actu.js
+++ build/models/actu.js
@@ -3,7 +3,7 @@ var Actu = (function () {
     function Actu() {
     }
     Actu.prototype.toString = function () {
-        return '{"postId":"' + this.postId + '","titre":"' + this.titre + '","texte":"' + this.texte + '","url":"' + this.url + '","image":"' + this.image + '","date":"' + Utils.formatDate(new Date(this.date)) + '"}';
+        return '{"postid":"' + this.postId + '","titre":"' + this.titre + '","texte":"' + this.texte + '","url":"' + this.url + '","image":"' + this.image + '","date":"' + Utils.formatDate(new Date(this.date)) + '"}';
     };
     return Actu;
 })();
