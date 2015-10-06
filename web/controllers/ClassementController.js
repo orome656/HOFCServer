d4b5b82d2bdd70ae882579c18580653bb5e2f8de
Orome656 <anthony.maladot@gmail.com>
2015-10-06 16:52:25 +0200

    Table Classemennt with ordering

--- web/controllers/ClassementController.js
+++ web/controllers/ClassementController.js
@@ -1,6 +1,13 @@
 app.controller('ClassementController',['$scope', 'classementService', function($scope, classementService) {
 	$scope.ranking = classementService.classement;
+	$scope.query = {
+        filter: '',
+        order: ['-points', '-diff']
+    };
 	
+	$scope.onChange = function (order) {
+		$scope.query.order = order
+	};
 	classementService.getClassement();
 	
 }]);
--- web/services/ClassementService.js
+++ web/services/ClassementService.js
@@ -1,17 +1,25 @@
 
 app.service('classementService', ['$q', '$http', function($q, $http) {
 	var _classement = [];
- 
+    
     var _getClassement = function(){
         $http.get("/classement")
             .then(function(results){
                 //Success
+                var datas = [];
+                for(var i in results.data) {
+                    for(var j in results.data[i]) {
+                        if(!isNaN(results.data[i][j])) {
+                            results.data[i][j] = parseInt(results.data[i][j]);
+                        }
+                    }
+                }
                 angular.copy(results.data, _classement); //this is the preferred; instead of $scope.movies = result.data
             }, function(results){
                 //Error  
             })
     }
-	
+    
 	return{
         classement: _classement,
         getClassement: _getClassement
